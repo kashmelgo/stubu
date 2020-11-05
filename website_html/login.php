8c29ed67ef011350e0ca862a1c8d556b28f75d0b
johndaves6240 <73985344+johndaves6240@users.noreply.github.com>
2020-11-05 19:03:44 +0800

    font changes
    font changes

--- website_html/login.php
+++ website_html/login.php
@@ -12,6 +12,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="style.css">
 <link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 
 
 </head>
