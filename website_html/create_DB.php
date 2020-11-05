4dc8d7605cc0c8d404d852112fcd1fa6fb4b005b
PokenChupin <73985157+PokenChupin@users.noreply.github.com>
2020-11-05 16:29:27 +0800

    Update create_DB.php

--- website_html/create_DB.php
+++ website_html/create_DB.php
@@ -4,27 +4,29 @@
     	<title>Create Database</title>
     </head>
     <body>
-    	<h1>Para buhat ni ug Database haa???</h1>
+    	<h1>Create Database Yayaya</h1>
     	<?php
             $conn = new mysqli("localhost","root","");                 // Directory Location
-            $select = $conn->select_db("STUBU_DATABASE");           // Connect to Database
+            $select = $conn->select_db("Stubu_Database");           // Connect to Database
             if(!$select){                                              // Creates Database if Database does not Exist
-                $conn->query("CREATE DATABASE STUBU_DATABASE;");    // Add Database Name
-                    $conn->select_db('STUBU_DATABASE');                    // Table and Columns Creation
+                $conn->query("CREATE DATABASE Stubu_Database;");    // Add Database Name
+                    $conn->select_db('Stubu_Database');                    // Table and Columns Creation
                     $conn->query("CREATE TABLE User(
                     User_ID INT AUTO_INCREMENT PRIMARY KEY,
                     Username VARCHAR(100) NOT NULL,
-                    Password VARCHAR(50) NOT NULL,
+                    Password NVARCHAR(50) NOT NULL,
                     Email_address VARCHAR(50) NOT NULL,
                     Profile_picture VARCHAR(100) NOT NULL,
                     First_Name VARCHAR(50) NOT NULL,
                     Last_Name VARCHAR(50) NOT NULL,
                     Mobile_Number INT NOT NULL,
-                    Date_Created DATE NOT NULL;
-                    Last_online DATE NOT NULL;
-                    User_level INT NOT NULL; 
+                    Date_Created DATE NOT NULL,
+                    Last_online DATE NOT NULL,
+                    User_level INT NOT NULL
                     )"                   // <----------- Directory Column
                 );
+
+                echo "<br>Database successfully Created!";
             }
         ?>
     </body>
