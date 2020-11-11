4c29a441295fa1d28e734e6569148d1404a19cba
PokenChupin <73985157+PokenChupin@users.noreply.github.com>
2020-11-11 14:16:18 +0800

    Naa nay color logout HEHE

--- website_html/displayProfile.php
+++ website_html/displayProfile.php
@@ -97,8 +97,7 @@
     <ul class="nav navbar-nav navbar-right">
       <li><a href="displayProfile.php" style="color: white;">Home</a></li>
       <li><a href="displayProfile.php" style="color: white;">Profile</a></li>
-
-      <li><a><form method="POST" action=""><input type="submit" name="logout" value="Log Out" style="color: white;"></form></a></li>
+      <li><a><form method="POST" action=""><input type="submit" name="logout" value="Log Out" style="color: white;border: none;background-color: #00789E;"></form></a></li>
 
     
 
