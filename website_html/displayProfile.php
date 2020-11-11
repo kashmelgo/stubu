8ad00cc0c5aaab67c0c14cfdaba6ddc4064fd768
johndaves6240 <73985344+johndaves6240@users.noreply.github.com>
2020-11-11 13:16:16 +0800

    profile changes

--- website_html/displayProfile.php
+++ website_html/displayProfile.php
@@ -34,7 +34,7 @@
 	        }
         }
     ?>
-<div class ="main-container">
+<!-- <div class ="main-container">
     <div class = "col-md-4" id ="leftSide">
         <div id="leftoSide">   
             <h1> StuBu</h1>
@@ -76,7 +76,45 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header" id="stubu">
+    	<h1>StuBu</h1>
+    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+    		<span class="icon-bar"></span>
+    		<span class="icon-bar"></span>
+    		<span class="icon-bar"></span>
+    	</button>
+      	
+    </div>
+    <div class="collapse navbar-collapse" id="micon">
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="index.html" style="color: white;">Home</a></li>
+      <li><a href="hobbies.html"style="color: white;">Profile</a></li>
+      <li><a href="interest.html"style="color: white;">Logout</a></li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-sm-6">
+				<img src="defaultpic.png" class="img-responsive" width="500px">
+			</div>
+
+			<div class="col-sm-6">
+				<h1>About Me</h1>
+				<p> Good day!</p>
+				<p>Nunc ut neque eget ante pretium scelerisque vitae id mauris. Etiam lacinia tristique nisi, sed vehicula tellus iaculis nec. Maecenas maximus metus hendrerit, finibus magna sed, tempor nulla. Pellentesque quam tortor, iaculis in ullamcorper at, imperdiet eu risus. Pellentesque bibendum ipsum eu volutpat facilisis. Vivamus justo nisi, vestibulum ut pellentesque eget, maximus sed nisi. Morbi quam orci, auctor vitae ullamcorper sit amet, tristique at diam. Pellentesque volutpat, neque quis euismod tempus, magna ex lobortis sapien, nec fringilla eros dui eget mi. Donec posuere viverra elit, sed pharetra quam auctor eget. Vestibulum pellentesque, sapien quis elementum semper, nunc felis vulputate nulla, sit amet venenatis neque ligula sit amet nulla. Mauris dolor ex, commodo sit amet velit sit amet, vulputate feugiat sapien. Pellentesque lacinia turpis id vulputate laoreet. Mauris pellentesque consequat imperdiet. Praesent ante ex, accumsan vel nibh non, tristique rhoncus massa. Phasellus euismod iaculis purus nec ullamcorper. Nunc ac congue dui. </p>	
+			</div>
+
+
+		</div>
+	</div>
     
     
 </body>
