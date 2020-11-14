fdf93acf5c4f8bd2f326c25bf9640cbd0ca2dd8d
PokenChupin <73985157+PokenChupin@users.noreply.github.com>
2020-11-14 09:42:31 +0800

    Update signup.php

--- website_html/signup.php
+++ website_html/signup.php
@@ -71,35 +71,6 @@
                         }
                     }
                 }
-
-                /*
-                $username = $_POST['user_name'];  
-                $firstname = $_POST['first_name'];
-                $lastname = $_POST['last_name'];
-                $password = $_POST['pass_word'];  
-                $email = $_POST['email_address'];
-                $datecreated = date("Y-m-d");
-                $lastlogin = date("Y-m-d");
-                $mobileNumber = $_POST['mobile_number'];
-                $sql = "INSERT INTO User VALUES ('','$username','$password','$email','defaultPic','$firstname','$lastname','$mobileNumber','$datecreated','$lastlogin','0')";
-                $conn->query($sql);                                  // Connect inputted data to database
-
-                $result = $conn->query("SELECT * FROM User WHERE Username = '$username'");
-                if($row = $result->fetch_assoc()){  
-                    $_SESSION['user_id'] = $row['User_ID'];
-                    $_SESSION['username'] = $row['Username'];
-                    $_SESSION['password'] = $row['Password'];
-                    $_SESSION['email'] = $row['Email_address'];
-                    $_SESSION['profile_picture'] = $row['Profile_picture'];
-                    $_SESSION['first_name'] = $row['First_Name'];
-                    $_SESSION['last_name'] = $row['Last_Name'];
-                    $_SESSION['mobile_number'] = $row['Mobile_Number'];
-                    $_SESSION['date_created'] = $row['Date_Created'];
-                    $_SESSION['last_online'] = $row['Last_online'];
-                    $_SESSION['user_level'] = $row['User_level'];
-                    header("Location: displayProfile.php");
-                } 
-                */  
             }
         }
     ?>
