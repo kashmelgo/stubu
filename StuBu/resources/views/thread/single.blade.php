03814cb4d364710dc75789a1a32dd1290c9079fd
PokenChupin <jrcelso2911@gmail.com>
2020-11-26 09:23:05 +0800

    Naa nay Modal ang Comment

--- StuBu/resources/views/thread/single.blade.php
+++ StuBu/resources/views/thread/single.blade.php
@@ -24,23 +24,40 @@
             <h4>{{$comment->user->name}}</h4>
             <p>{{$comment->body}}</p>
             <div class="actions">
-                <!--<a href="{{route('thread.edit',$thread->id)}}" class="btn btn-info btn-xs">Edit</a> -->
 
-                <a class="btn btn-xs" data-toggle="modal" href="{{$comment->id}}">Edit</a>
-                <div class="modal fade" id="{{$comment->id}}">
-                    <div class="comment-form">
-                        <form action="{{route('comment.update',$comment->id)}}" method="POST" role="form">
-                            {{csrf_field()}}
-                            {{method_field('put')}}
-                            <legend>Edit Comment</legend>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="body" id="" placeholder="Input..." value="{{$comment->body}}">
+                <a type="btn btn-xs" class="btn btn-primary" data-toggle="modal" href="#edit-comment-{{ $comment->id }}">
+                    Edit
+                </a>
+
+                <div class="modal fade" id="edit-comment-{{ $comment->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalCenterTitle">Edit Comment</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="comment-form">
+                                <form action="{{route('comment.update',$comment->id)}}" method="POST" role="form">
+                                    {{csrf_field()}}
+                                    {{method_field('put')}}
+                                    <legend>Edit Comment</legend>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="body" id="" placeholder="Input..." value="{{$comment->body}}">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
                     </div>
                 </div>
-
                 <form action="{{ route('comment.destroy',$comment->id) }}" method="POST" class="inline-it">
                     {{csrf_field()}}
                     {{method_field('DELETE')}}
