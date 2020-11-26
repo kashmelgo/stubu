7682cceb4d430b7cadcd358ec54494b89b1c33f0
PokenChupin <jrcelso2911@gmail.com>
2020-11-26 11:40:53 +0800

    Akoa remove tryDaw

--- stubu/routes/web.php
+++ stubu/routes/web.php
@@ -18,17 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('welcome',function(){
-    return view('welcome');
-})->name('tryDaw');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/homefeed', [App\Http\Controllers\HomeFeedController::class, 'index'])->name('homefeed');
 
-//Route::get( '/thread', [App\Http\Controllers\ThreadController::class, 'index'])->name('thread');
 Route::resource('/thread','App\Http\Controllers\ThreadController');
 
 Route::resource('comment','App\Http\Controllers\CommentController',['only'=>['update','destroy']]);
