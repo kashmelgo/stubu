3c5d4c6cb3e090dff31d9f60adfe61b719248f42
PokenChupin <jrcelso2911@gmail.com>
2020-12-21 17:04:20 +0800

    Trials Sa Like

--- StuBu/app/Models/Like.php
+++ StuBu/app/Models/Like.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $guarded=[];
+
+    public function likable()
+    {
+        return $this->morphTo();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
