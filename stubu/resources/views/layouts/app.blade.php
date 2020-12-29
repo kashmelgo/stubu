19901fdfd80e2e4d13f5a200bacaee497061ec44
kashmelgo <kashmelg@gmail.com>
2020-12-29 14:31:31 +0800

    Update app.blade.php

--- stubu/resources/views/layouts/app.blade.php
+++ stubu/resources/views/layouts/app.blade.php
@@ -32,13 +32,22 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
             <div class="container">
+                @auth
                 <a id = "brand-name" class="navbar-brand" href="{{ url('/homefeed') }}">
                     {{ config('app.name', 'StuBu') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                @endauth
+                @guest
+                <a id = "brand-name" class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'StuBu') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                @endguest
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
